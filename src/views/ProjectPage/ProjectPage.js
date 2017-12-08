29553e2138e187c7d04175a96e985c63e3ab3ac1
KevinKleine1 <kevin_kleine@web.de>
2017-12-08 15:49:01 +0100

    Deleting dead code

--- src/views/ProjectPage/ProjectPage.js
+++ src/views/ProjectPage/ProjectPage.js
@@ -27,10 +27,6 @@ import {Link} from 'react-router-dom';
 
 var currentPageUrl = window.location.href;
 var currentPageUrlShort = window.location.host + window.location.pathname;
-var karma = 800;
-var header = 'Header';
-var descirption = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi';
-
 //Social media redirects
 var redirectToTwitter = 'https://twitter.com/intent/tweet?text=Hey,%20schau%20dir%20dieses%20Projekt%20an:%20' + currentPageUrl
 var redirectToGooglePlus = 'https://plus.google.com/share?url=' + currentPageUrl
