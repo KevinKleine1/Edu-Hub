b2ab83b0119badcbdc8c67bb282cec751b8a5914
Ömer Ülker <omerulker@Omers-MacBook-Pro.local>
2017-12-15 12:57:17 +0100

    fixed sorting problems in the table

--- src/views/MyProjects/MyProjects.js
+++ src/views/MyProjects/MyProjects.js
@@ -2,39 +2,28 @@ import _ from 'lodash';
 import React, {Component} from 'react';
 import {Table, Button} from 'semantic-ui-react';
 
-//Bug: Datum wird nicht richtig sortiert
-
-// formates date from database to readable
-function formatDate(date_unformatted){
-var day = date_unformatted.substr(8, 2);
-var month = date_unformatted.substr(5, 2);
-var year = date_unformatted.substr(0, 4);
-var date_formatted = day + '.' + month + '.' + year;
-return date_formatted;
-}
-
-const colors = ['blue']
+//This part needs to be created dynamicly according to user
 const tableData = [
   {
     Nummer: '1',
     ProjektName: 'Lernzentrum',
     Status: 'In Bearbeitung',
-    ZuletztVeraendert: '2017.10.09'
+    ZuletztVeraendert: '2017-01-08T15:46:00.000Z'
   }, {
     Nummer: '2',
     ProjektName: 'Fitness Studio',
     Status: 'In Bearbeitung',
-    ZuletztVeraendert: '2016.11.10'
+    ZuletztVeraendert: '2016-12-07T15:46:00.000Z'
   }, {
     Nummer: '3',
     ProjektName: 'Coding Kurs',
     Status: 'Fertig',
-    ZuletztVeraendert: '2017.09.10'
+    ZuletztVeraendert: '2017-12-04T15:46:00.000Z'
   }, {
     Nummer: '4',
     ProjektName: 'Bundes Jugendspiele',
     Status: 'Fertig',
-    ZuletztVeraendert: '2016.10.09'
+    ZuletztVeraendert: '2017-12-04T15:45:00.000Z'
   }
 ]
 
@@ -44,7 +33,50 @@ class MyProjects extends React.Component {
     data: tableData,
     direction: null
   }
+  /* formates date from database to readable | currently unused in this view
+  formatDate(date_unformatted){
+  var day = date_unformatted.substr(8, 2);
+  var month = date_unformatted.substr(5, 2);
+  var year = date_unformatted.substr(0, 4);
+  return day + '.' + month + '.' + year;
+}*/
 
+  // formates date with time
+  formatDateWithTime(date_unformatted){
+  var day = date_unformatted.substr(8, 2);
+  var month = date_unformatted.substr(5, 2);
+  var year = date_unformatted.substr(0, 4);
+  var time = date_unformatted.substr(11, 5);
+  var monthNamed = null;
+  if(month == '01'){
+    monthNamed = 'Jan';
+  }else if(month == '02'){
+    monthNamed = 'Feb';
+  }else if(month == '03'){
+    monthNamed = 'Mrz';
+  }else if(month == '04'){
+    monthNamed = 'Apr';
+  }else if(month == '05'){
+    monthNamed = 'Mai';
+  }else if(month == '06'){
+    monthNamed = 'Jun';
+  }else if(month == '07'){
+    monthNamed = 'Jul';
+  }else if(month == '08'){
+    monthNamed = 'Aug';
+  }else if(month == '09'){
+    monthNamed = 'Sep';
+  }else if(month == '10'){
+    monthNamed = 'Okt';
+  }else if(month == '11'){
+    monthNamed = 'Nov';
+  }else{
+    monthNamed = 'Dez';
+  }
+  return day + '. ' + monthNamed + '. ' + year + ' ' + time
+  }
+
+//sorts column
   handleSort = clickedColumn => () => {
     const {column, data, direction} = this.state
 
@@ -72,7 +104,7 @@ class MyProjects extends React.Component {
     return (
 
 <div>
-      <Table sortable={true} celled={true} fixed={true} selectable={true}>
+      <Table sortable={true} celled={true} fixed={true} selectable={true} size='large'>
       <Table.Header>
         <Table.Row>
           <Table.HeaderCell sorted={column === 'Nummer'
@@ -103,7 +135,7 @@ class MyProjects extends React.Component {
             <Table.Cell>{Nummer}</Table.Cell>
             <Table.Cell>{ProjektName}</Table.Cell>
             <Table.Cell>{Status}</Table.Cell>
-            <Table.Cell>{ZuletztVeraendert}</Table.Cell>
+            <Table.Cell>{this.formatDateWithTime(ZuletztVeraendert)}</Table.Cell>
           </Table.Row>))
         }
       </Table.Body>
