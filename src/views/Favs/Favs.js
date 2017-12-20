90ee05a9354fbda0689b230d2b3f443effe5ada0
Burcu Akcay <burcu@Burcus-MacBook-Air.local>
2017-12-20 16:13:17 +0100

    Favorisierte Projekte

--- src/views/Favs/Favs.js
+++ src/views/Favs/Favs.js
@@ -1,20 +1,15 @@
 import React, {Component} from 'react';
 import {
-  Row,
-  Col,
+  Header,
+  Icon,
+  Segment,
+  Divider,
   Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  CardTitle,
-  Button
-} from 'reactstrap';
+  Button,
+  Grid,
+  Image
+} from 'semantic-ui-react';
 
-const brandPrimary = '#20a8d8';
-const brandSuccess = '#4dbd74';
-const brandInfo = '#63c2de';
-const brandWarning = '#f8cb00';
-const brandDanger = '#f86c6b';
 
 class Favs extends Component {
   constructor(props) {
@@ -36,85 +31,73 @@ class Favs extends Component {
 
     return (<div className="animated fadeIn">
       <div className="container">
-        <div className="row">
-          <div className="col">
-            <div class="card" style={{
-                width: "20rem"
-              }}>
-              <img class="card-img-top" src="https://i.imgur.com/ockpsKj.jpg" alt="Card image cap"></img>
-              <div class="card-body">
-                <h4 class="card-title">Titel</h4>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card" style={{
-                width: "20rem"
-              }}>
-              <img class="card-img-top" src="https://i.imgur.com/zs0xJsA.jpg" alt="Card image cap"></img>
-              <div class="card-body">
-                <h4 class="card-title">Titel</h4>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card" style={{
-                width: "20rem"
-              }}>
-              <img class="card-img-top" src="https://i.imgur.com/G6N2paL.jpg" alt="Card image cap"></img>
-              <div class="card-body">
-                <h4 class="card-title">Titel</h4>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card" style={{
-                width: "20rem"
-              }}>
-              <img class="card-img-top" src="https://i.imgur.com/uk2MB1c.jpg" alt="Card image cap"></img>
-              <div class="card-body">
-                <h4 class="card-title">Titel</h4>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card" style={{
-                width: "20rem"
-              }}>
-              <img class="card-img-top" src="https://i.imgur.com/ockpsKj.jpg" alt="Card image cap"></img>
-              <div class="card-body">
-                <h4 class="card-title">Titel</h4>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card" style={{
-                width: "20rem"
-              }}>
-              <img class="card-img-top" src="https://i.imgur.com/zGHOb6A.jpg" alt="Card image cap"></img>
-              <div class="card-body">
-                <h4 class="card-title">Titel</h4>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div class="card" style={{
-                width: "20rem"
-              }}>
-              <img class="card-img-top" src="https://i.imgur.com/ockpsKj.jpg" alt="Card image cap"></img>
-              <div class="card-body">
-                <h4 class="card-title">Titel</h4>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className="row justify-content-md-center">
+          <Segment vertical={true} style={{
+              width: "1000px"
+            }}>
+            <Header as='h2'>
+              <Icon name='empty heart'/>
+              <Header.Content>
+                Favoriten
+                <Header.Subheader>
+                  Projekte, die du favorisiert hast
+                </Header.Subheader>
+              </Header.Content>
+            </Header>
+            <Divider fitted/>
+        <Divider hidden/>
+        <Grid doubling={true} columns={4} divided='vertically'>
+          <Grid.Column>
+            <Image
+              fluid
+              label={{ as: 'a', color: 'teal', content: 'Digitale Bibliothek', ribbon: true }}
+              src='/img/Landingpage/projekt1.jpg'
+                />
+              </Grid.Column>
+              <Grid.Column>
+      <Image
+        fluid
+        label={{ as: 'a', color: 'teal', content: 'Experiment', ribbon: true }}
+        src='/img/Landingpage/projekt2.jpg'
+      />
+    </Grid.Column>
+    <Grid.Column>
+          <Image
+            fluid
+            label={{ as: 'a', color: 'teal', content: 'Selbstlernzetrum', ribbon: true }}
+            src='/img/Landingpage/projekt3.jpg'
+          />
+        </Grid.Column>
+        <Grid.Column>
+              <Image
+                fluid
+                label={{ as: 'a', color: 'teal', content: 'Cafeteria', ribbon: true }}
+                src='/img/Landingpage/projekt4.jpeg'
+              />
+            </Grid.Column>
+
+        </Grid>
+        <Grid doubling={true} columns={4} divided='vertically'>
+          <Grid.Column>
+            <Image
+              fluid
+              label={{ as: 'a', color: 'teal', content: 'Garten AG', ribbon: true }}
+              src='/img/Landingpage/projekt5.jpeg'
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Image
+                  fluid
+                  label={{ as: 'a', color: 'teal', content: 'KFZ Werkstatt', ribbon: true }}
+                  src='/img/Landingpage/projekt6.jpeg'
+                />
+      </Grid.Column>
+
+
+        </Grid>
+
+      </Segment>
+    </div>
       </div>
 
     </div>)
--- src/views/Test2/Test2.js
+++ src/views/Test2/Test2.js
@@ -1,140 +1,106 @@
 import React, {Component} from 'react';
-import Auth from '../../Auth/Auth';
-import history from '../../history';
-import {Link, Switch, Route, Redirect} from 'react-router-dom';
 import {
   Header,
   Icon,
-  Image,
-  Container,
-  Card,
-  Grid,
-  Button,
-  Message,
-  Input,
-  Menu,
   Segment,
-  Divider
+  Divider,
+  Card,
+  Button,
+  Grid,
+  Image
 } from 'semantic-ui-react';
-import Welcome from '../Pages/Welcome/Welcome';
-import ProjectCards from '../../components/ProjectCards/ProjectCards';
-import 'whatwg-fetch'
 
-const auth = new Auth();
 
-//dashboard class where we can see up to date project and which is in general our landing page
-class Test2 extends React.Component {
-
+class Test2 extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      modal: false,
-      activeItem: '',
-      dropdownOpen: false,
-      activeItem: "Kernprojekte",
-      Data: []
-    };
 
     this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
   }
 
-  //Class to create a new project card with all the necessary data
-  createImage(image) {
-    return <ProjectCards name={image.project_name} members={image.project_membercount} text={image.project_text} bild={image.project_imagepath} erstellt={image.project_created_at} link={image.projectid} key={image.projectid}/>;
-  }
-
-  //this is the mapping class which uses createImage on every content of the array
-  createImages(images, start, end) {
-    var Plist = images.slice(start, end)
-    return Plist.map(this.createImage);
-
-  }
-  //handler for the menu on top to change categories
-  handleItemClick = (e, {name}) => this.setState({activeItem: name})
-
-  //dropdown handler
   toggle() {
     this.setState({
       dropdownOpen: !this.state.dropdownOpen
-
     });
   }
 
-  //fetch call to get all projects we have available atm TODO: Remove subprojects, make it dynamically, have some kind of sorting
-  getProjects() {
-    var target = ('http://backend-edu.azurewebsites.net/project/')
-    fetch(target).then((results) => {
-      return results.json();
-
-    }).then((json) => {
-
-      this.setState({
-        Data: json
-      }, function() {
-        this.setState({
-          Id: this.state.Data.map((elem) => elem.projectid)
-        }, function() {});
-      })
-
-    })
-  }
-
-  newProject() {
-    history.replace('/wizard1');
-  }
-
-  isAuthenticated() {
-    // Check whether the current time is past the
-    // access token's expiry time
-    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-    return new Date().getTime() < expiresAt;
-  }
-  //inital call after component did render
-  componentDidMount() {
-    this.getProjects();
-
-  }
-
   render() {
-    const logged = this.isAuthenticated();
-    const {activeItem} = this.state;
 
     return (<div className="animated fadeIn">
-<div className="row justify-content-md-center">
-  <div className="position-relative">
-
-        <img className="banner" src='../img/EduBanner.png'/>
-        <Button basic={true} color='grey' style={{
-          position: 'relative',
-            width: '15%',
-            height: 'auto',
-            left: '320px',
-            bottom: '50px'
-          }}>
-          <b>Einloggen/ Registrieren</b>
-        </Button></div>
-    </div>
       <div className="container">
         <div className="row justify-content-md-center">
-          <Grid doubling={true} columns={4} divided='vertically'>
-             <Container textAlign='right'>
-               <Header as='h2' color='grey' floated='left'>
-                 Projekte entdecken
-               </Header>
-            <Input size='huge' transparent icon={{ name: 'search', link: true }} placeholder='Projekt suchen...' />
-          </Container>
-              <Grid.Row>
-              {this.createImages(this.state.Data, 0, 4)}
-            </Grid.Row>
-          </Grid>
-          <Grid doubling={true} columns={4} divided='vertically'>
-            <Grid.Row>
-              {this.createImages(this.state.Data, 4, 8)}
-            </Grid.Row>
-          </Grid>
-        </div>
+          <Segment vertical={true} style={{
+              width: "1000px"
+            }}>
+            <Header as='h2'>
+              <Icon name='empty heart'/>
+              <Header.Content>
+                Favoriten
+                <Header.Subheader>
+                  Projekte, die du favorisiert hast
+                </Header.Subheader>
+              </Header.Content>
+            </Header>
+            <Divider fitted/>
+        <Divider hidden/>
+        <Grid doubling={true} columns={4} divided='vertically'>
+          <Grid.Column>
+            <Image
+              fluid
+              label={{ as: 'a', color: 'teal', content: 'Digitale Bibliothek', ribbon: true }}
+              src='/img/Landingpage/projekt1.jpg'
+                />
+              </Grid.Column>
+              <Grid.Column>
+      <Image
+        fluid
+        label={{ as: 'a', color: 'teal', content: 'Experiment', ribbon: true }}
+        src='/img/Landingpage/projekt2.jpg'
+      />
+    </Grid.Column>
+    <Grid.Column>
+          <Image
+            fluid
+            label={{ as: 'a', color: 'teal', content: 'Selbstlernzetrum', ribbon: true }}
+            src='/img/Landingpage/projekt3.jpg'
+          />
+        </Grid.Column>
+        <Grid.Column>
+              <Image
+                fluid
+                label={{ as: 'a', color: 'teal', content: 'Cafeteria', ribbon: true }}
+                src='/img/Landingpage/projekt4.jpeg'
+              />
+            </Grid.Column>
+
+        </Grid>
+        <Grid doubling={true} columns={4} divided='vertically'>
+          <Grid.Column>
+            <Image
+              fluid
+              label={{ as: 'a', color: 'teal', content: 'Garten AG', ribbon: true }}
+              src='/img/Landingpage/projekt5.jpeg'
+                />
+              </Grid.Column>
+              <Grid.Column>
+                <Image
+                  fluid
+                  label={{ as: 'a', color: 'teal', content: 'KFZ Werkstatt', ribbon: true }}
+                  src='/img/Landingpage/projekt6.jpeg'
+                />
+      </Grid.Column>
+
+
+        </Grid>
+
+      </Segment>
+    </div>
       </div>
-    </div>);
+
+    </div>)
   }
 }
 
